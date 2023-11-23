0add7f86886041bf92532349592e3c38881e0913
Yoichi Ojima <yoichiojima@gmail.com>
2023-11-23 16:35:29 +0900

    django tutorial

---
+++ polls/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length = 200)
+    pub_date = models.DateTimeField("date published")
+
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete = models.CASCADE)
+    choices_text = models.CharField(max_length = 200)
+    votes = models.IntegerField(default = 0)
